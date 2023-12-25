6221f01319e3446663d45f8fa76f96702c9e4470
Liliana Franco <lilimafra@hotmail.com>
2023-12-24 21:47:27 -0500

    background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { makeStyles } from "@material-ui/core";
 
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <h1> My portfolio</h1>
     </div>
   );
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    
+  }
+}))
 
 export default App;
